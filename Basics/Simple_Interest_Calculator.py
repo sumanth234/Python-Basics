676849952ca3872c3b408a96e47871e9c9b1e67d
sumanthu <funnyfunko@gmail.com>
2020-06-29 00:55:04 +0530

    initial commit

---
+++ Basics/Simple_Interest_Calculator.py
@@ -0,0 +1,11 @@
+p = input('Please Enter the Principal');
+t = input('Please Enter the Number of years');
+r = input('Please Enter the rate of Interest');
+
+p = int(p)
+t = int(t)
+r = int(r)
+
+si = (p*t*r)/100
+
+print(si);
---
+++ Basics/if_condition.py
@@ -0,0 +1,8 @@
+age = int(input('Please enter your age'))
+
+if age >= 60:
+    print('Senior Citizen')
+elif age >= 18:
+    print('Adult')
+else:
+    print('Children');
